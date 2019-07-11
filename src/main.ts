e1e96b08d7d566b8bb9e78a7d77f72486514bb1e
Filipe Silva <filipematossilva@gmail.com>
2019-07-11 15:04:18 +0100

    use angular and material next

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,8 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+import '@angular/animations';
+import '@angular/forms';
+import '@angular/router';
+import '@angular/material';
